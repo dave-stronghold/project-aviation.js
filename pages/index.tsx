b25205e8587a62c2e136372e5f0cfe30830e4977
dave stronghold <davestronghold@hotmail.com>
2023-01-23 17:17:29 +0000

    Timer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,24 @@
-import type { NextPage } from 'next'
-import { useEffect, useState } from 'react'
+import type { NextPage } from "next";
+import { useEffect, useState } from "react";
 
-import styles from '../styles/Home.module.css'
+import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  const[alphabet,setAlphabet]=useState('VT')
-  useEffect(()=>{
-    
-  },[])
+  const [alphabet, setAlphabet] = useState(0);
+
+  useEffect(() => {
+    const changeValue = () => {
+      setAlphabet(alphabet + 1);
+    };
+    const interval = setInterval(changeValue, 1000);
+    return () => clearInterval(interval);
+  }, [alphabet]);
+
   return (
     <div className={styles.container}>
-     <h1 className={styles.title}>
-       {alphabet}
-     </h1>
-
-     
+      <h1 className={styles.title}>{alphabet}</h1>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
